d1ea37ffce552474a3509ee01ac3f96b006fdce4
Rychy248 <jorgeajrhh@gmail.com>
2024-09-20 10:10:51 -0600

    fix(app.ui.page.tsx)charSet fix

--- app/ui/page.tsx
+++ app/ui/page.tsx
@@ -4,7 +4,7 @@ function Name() {
   return(
     <html lang="en">
     <head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Document</title>
     </head>
