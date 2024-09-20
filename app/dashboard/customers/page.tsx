72f3fd0070833d3fab0bd09a6f6ef73b2647531b
Rychy248 <jorgeajrhh@gmail.com>
2024-09-20 09:05:55 -0600

    initial dependendecis

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+function Customer() {
+  return <>
+    <h1>Customes Page</h1>
+  </>
+};
+
+export default Customer;
---
+++ app/ui/page.tsx
@@ -0,0 +1,18 @@
+
+
+function Name() {
+  return(
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>HELLO WOLRD!</h1>
+    </body>
+    </html>
+  );
+};
+
+export default Name;
